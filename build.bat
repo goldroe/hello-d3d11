6d1bb18c6ac6f88093f87489a7ec888dfbb15d5f
Carlos Rivera <cr4185320@gmail.com>
2022-12-13 20:29:53 -0600

    C++20 flag

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
 @echo off
 
 SET WarningFlags=-W4 -WX -wd4100 -wd4101 -wd4189
-SET CompilerFlags=-nologo -FC -Zi /MTd %WarningFlags% -FeGraphics.exe
+SET CompilerFlags=-nologo -FC -Zi /MTd %WarningFlags% -FeGraphics.exe -std:c++20
 
 PUSHD bin
 CL %CompilerFlags% ..\src\main.cpp
