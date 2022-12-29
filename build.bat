14afc4c62082ad8ad8c93efc041a162f09c36103
Carlos Rivera <cr4185320@gmail.com>
2022-12-28 23:24:53 -0600

    Added compiler warning flags

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
 @echo off
 
-SET WarningFlags=-W4 -WX -wd4100 -wd4101 -wd4189
-SET CompilerFlags=-nologo -FC -Zi /MTd %WarningFlags% -FeGraphics.exe -std:c++20
+SET WarningFlags=-W4 -WX -wd4100 -wd4101 -wd4189 -wd4996 -wd4530
+SET CompilerFlags=-nologo -FC -Zi /MTd %WarningFlags% -FeGraphics.exe
 
 PUSHD bin
 CL %CompilerFlags% ..\src\main.cpp
